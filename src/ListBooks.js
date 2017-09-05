52f8983b3735e6ad8c657497e4af8e1837c2eb60
Thomas Moser <mail@thomasmoser.ch>
2017-09-05 11:07:04 +0200

    Refactored Listing of bookshelfs to reduce duplicate HTML

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,7 +3,17 @@ import React, {Component} from 'react'
 class ListBooks extends Component {
 
   render(){
+
     const { books } = this.props;
+
+    console.log(books);
+
+    const bookshelfs = [
+      {value: "currentlyReading", title: "Currently Reading"},
+      {value: "wantToRead", title: "Want to Read"},
+      {value: "read", title: "Read"},
+      {value: "none", title: "None"}
+    ]
 
     return (
       <div className="list-books">
@@ -12,142 +22,38 @@ class ListBooks extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {
-                    /* TODO: Refactor this, so that the same HTML is NOT repeated for each bookshelf.
-                    *  Duplicate HTML is evil!
-                    */
-                  }
-                  { /* Loop all Books in the shelf currentlyReading */
-                    books.filter((book) => book.shelf==="currentlyReading").map((book) => (
-                      <li key={book.title}>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select defaultValue={book.shelf}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading" >Currently Reading</option>
-                                <option value="wantToRead" >Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
+            { /* Loop through bookshelfes */
+              bookshelfs.map((bookshelf)=>(
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{bookshelf.title}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      { /* Loop through books in the bookshelf */
+                        books.filter((book) => book.shelf===bookshelf.value).map((book) => (
+                          <li key={book.title}>
+                            <div className="book">
+                              <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                <div className="book-shelf-changer">
+                                  <select defaultValue={book.shelf}>
+                                    <option value="none" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead" >Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                  </select>
+                                </div>
+                              </div>
+                              <div className="book-title">{book.title}
+                              </div>
+                              <div className="book-authors">{book.authors.join(", ")}</div>
                             </div>
-                          </div>
-                          <div className="book-title">{book.title}
-                          </div>
-                          <div className="book-authors">{book.authors.join(", ")}</div>
-                        </div>
-                      </li>
-                    )) }
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {
-                    /* TODO: Refactor this, so that the same HTML is NOT repeated for each bookshelf.
-                    *  Duplicate HTML is evil!
-                    */
-                  }
-                  { /* Loop all Books in the shelf wantToRead */
-                    books.filter((book) => book.shelf==="wantToRead").map((book) => (
-                      <li key={book.title}>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select defaultValue={book.shelf}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading" >Currently Reading</option>
-                                <option value="wantToRead" >Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}
-                          </div>
-                          <div className="book-authors">{book.authors.join(", ")}</div>
-                        </div>
-                      </li>
-                    )) }
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {
-                    /* TODO: Refactor this, so that the same HTML is NOT repeated for each bookshelf.
-                    *  Duplicate HTML is evil!
-                    */
-                  }
-                  { /* Loop all Books in the shelf wantToRead */
-                    books.filter((book) => book.shelf==="read").map((book) => (
-                      <li key={book.title}>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select defaultValue={book.shelf}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading" >Currently Reading</option>
-                                <option value="wantToRead" >Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}
-                          </div>
-                          <div className="book-authors">{book.authors.join(", ")}</div>
-                        </div>
-                      </li>
-                    )) }
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">None</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {
-                    /* TODO: Refactor this, so that the same HTML is NOT repeated for each bookshelf.
-                    *  Duplicate HTML is evil!
-                    */
-                  }
-                  { /* Loop all Books in the shelf wantToRead */
-                    books.filter((book) => book.shelf==="none").map((book) => (
-                      <li key={book.title}>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select defaultValue={book.shelf}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading" >Currently Reading</option>
-                                <option value="wantToRead" >Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}
-                          </div>
-                          <div className="book-authors">{book.authors.join(", ")}</div>
-                        </div>
-                      </li>
-                    )) }
-                </ol>
-              </div>
-            </div>
+                          </li>
+                        )) }
+                    </ol>
+                  </div>
+                </div>
+              ))}
           </div>
         </div>
         <div className="open-search">
